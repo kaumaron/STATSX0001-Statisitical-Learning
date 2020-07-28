0fe857264809a702793302d6e592ee53d109497b
Andrew K DeCotiis-Mauro <andrewkmauro@gmail.com>
2020-07-28 18:59:32 -0400

    Finished commenting

--- Simple Linear Regression/Simple Linear Regression.R
+++ Simple Linear Regression/Simple Linear Regression.R
@@ -7,7 +7,8 @@ library(ISLR)
 # Simple Linear Regression
 
 # display dataframe in R Data Editor
-fix(Boston)
+# skipped because it's unneccessary in reruns but good for EDA
+# fix(Boston)
 
 # display column names from dataframe (similar to df.columns)
 names(Boston)
@@ -36,20 +37,35 @@ summary(lm.fit)
 #            Estimate Std. Error    t value  Pr(>|t|)    
 #(Intercept)   34.55384    0.56263   61.41   <2e-16 ***
 #  lstat       -0.95005    0.03873  -24.53   <2e-16 ***
-  
+
+# get names of model components  
 names(lm.fit)
+
+# get model coefficients and confidence interval
 coef(lm.fit)
 confint(lm.fit)
+
+# make predictions with the linear model
 predict(lm.fit,data.frame(lstat=(c(5,10,15))), interval="confidence")
 predict(lm.fit,data.frame(lstat=(c(5,10,15))), interval="prediction")
+
+# plot lstat, medv scatter
 plot(lstat,medv)
+# add straight line of model with abline
 abline(lm.fit)
+# tweak line style
 abline(lm.fit,lwd=3)
 abline(lm.fit,lwd=3,col="red")
+
+# tweak plot styles
 plot(lstat,medv,col="red")
 plot(lstat,medv,pch=20)
 plot(lstat,medv,pch="+")
+
+# display plot point styles
 plot(1:20,1:20,pch=1:20)
+
+# Set graph parameters to be a 2x2 set of subplots
 par(mfrow=c(2,2))
 plot(lm.fit)
 plot(predict(lm.fit), residuals(lm.fit))
@@ -63,11 +79,17 @@ lm.fit=lm(medv~lstat+age,data=Boston)
 summary(lm.fit)
 lm.fit=lm(medv~.,data=Boston)
 summary(lm.fit)
+
 library(car)
+# vif calculates variance-inflation and generalized
+# variance-inflation factors for linear, generalized 
+# linear, and other models.
 vif(lm.fit)
 lm.fit1=lm(medv~.-age,data=Boston)
 summary(lm.fit1)
+# Update and re-fit model call
 lm.fit1=update(lm.fit, ~.-age)
+summary(lm.fit1)
 
 # Interaction Terms
 
@@ -75,6 +97,7 @@ summary(lm(medv~lstat*age,data=Boston))
 
 # Non-linear Transformations of the Predictors
 
+# add lstat^2
 lm.fit2=lm(medv~lstat+I(lstat^2))
 summary(lm.fit2)
 lm.fit=lm(medv~lstat)
@@ -87,21 +110,26 @@ summary(lm(medv~log(rm),data=Boston))
 
 # Qualitative Predictors
 
-fix(Carseats)
+#fix(Carseats)
 names(Carseats)
 lm.fit=lm(Sales~.+Income:Advertising+Price:Age,data=Carseats)
 summary(lm.fit)
 attach(Carseats)
 contrasts(ShelveLoc)
+?contrasts
 
 # Writing Functions
 
+# not yet created
 LoadLibraries
 LoadLibraries()
+# create a load libraries function that loads ISLR and MASS
 LoadLibraries=function(){
   library(ISLR)
   library(MASS)
   print("The libraries have been loaded.")
 }
+# shows the function definition
 LoadLibraries
+# calls function
 LoadLibraries()
