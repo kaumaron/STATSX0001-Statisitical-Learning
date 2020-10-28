9b285e967b5905120559bedd9497f24eae870d76
Andrew K DeCotiis-Mauro <andrewkmauro@gmail.com>
2020-10-28 11:35:43 -0400

    Answers for R1 and R2

--- Moving Beyond Linearity/Quiz Solutions.R
+++ Moving Beyond Linearity/Quiz Solutions.R
@@ -4,4 +4,16 @@ getwd()
 
 load('7.R.RData')
 
-View(V1)
+# 7.R.R1
+# Load the data from the file 7.R.RData, and plot it using plot(x,y).
+# What is the slope coefficient in a linear regression of y on x (to within 10%)?
+
+plot(x,y)
+lr = lm(y~x)
+summary(lr)$coefficients[2]
+
+# 7.R.R2
+# For the model y ~ 1+x+x^2, what is the coefficient of x (to within 10%)?
+
+qm = lm(y ~ 1+x+I(x^2))
+summary(qm)$coefficients[2]
